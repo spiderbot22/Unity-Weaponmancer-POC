e281ee5557b74ec4a54f81c57b36aa375f8cdf02
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-01-03 14:30:45 -0800

    removed old animations. New idle and walk (forward, back, left, right) animations rigged, looped, and baked.

--- Assets/Characters/Player/Scripts/PlayerController.cs
+++ Assets/Characters/Player/Scripts/PlayerController.cs
@@ -28,13 +28,24 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
 
-        //Moving
+        /*Movement*/
+
         if (movement.magnitude > 0)
         {
             movement.Normalize();
             movement *= moveSpeed * Time.deltaTime;
             transform.Translate(movement, Space.World);
         }
+
+        /*Animating*/
+
+        //grab forward/back and left/right velocity to pass to animator vars
+        float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
+        float velocityX = Vector3.Dot(movement.normalized, transform.right);
+
+        //pass velocity to animator vars
+        _animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime); //("param name", param value, smoothing time, delta time)
+        _animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
     }
 
     public void InputRead(InputAction.CallbackContext context)
