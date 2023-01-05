a51ab51df02c075c64e41517699774601db84fab
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-01-05 15:14:44 -0800

    Added small sphere to player root and used it in player script to detect ground for activating/deactivating drag. Updated script movement calculations to use serialized variables instead of numbers.

--- Assets/Characters/Player/Scripts/PlayerController.cs
+++ Assets/Characters/Player/Scripts/PlayerController.cs
@@ -6,16 +6,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] float moveSpeed;
     [SerializeField] LayerMask _aimLayerMask;
+    [Header("Movement")]
+    public float walkSpeed;
+    public float jogSpeed;
+    public float sprintSpeed;
+    public float moveSpeed;
+    public float groundDrag;
+    public float lookSensitivity;
     public Transform orientation;
-    Vector3 moveDirection;
-    Vector2 moveVector;
-    Rigidbody _rigidBody;
-    Animator _animator;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask whatIsGround;
+    bool grounded;
+    
+    private Vector3 moveDirection;
+    private Vector2 moveVector;
+    private Rigidbody _rigidBody;
+    private Animator _animator;
     private Vector2 lookVector;
-    private Vector3 rotation;
-    private float lookSensitivity = 5;
 
 
     // Start is called before the first frame update
@@ -27,6 +38,26 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Rotate();
+
+        /*Ground Check*/
+        grounded = Physics.Raycast(groundCheck.position, Vector3.down, groundDistance, whatIsGround);
+        Debug.Log(grounded);
+
+        /*Drag Handler*/
+        if (grounded)
+        {
+            _rigidBody.drag = groundDrag;
+        }
+        else
+        {
+            _rigidBody.drag = 0;
+        }
+
+    }
+
+    private void FixedUpdate()
     {
         Move();
     }
@@ -49,31 +80,25 @@ public class PlayerController : MonoBehaviour
         {
             movement.Normalize();
             movement *= moveSpeed * Time.deltaTime;
-            //_rigidBody.AddForce(movement * moveSpeed * 30f, ForceMode.Force);
-            _rigidBody.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+            _rigidBody.AddForce(moveDirection.normalized * moveSpeed * moveSpeed, ForceMode.Force);
 
         }
 
         /*Animating*/
 
         //grab forward/back and left/right velocity to pass to animator vars
-        //float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
-        //float velocityX = Vector3.Dot(movement.normalized, transform.right);
         float velocityZ = Vector3.Dot(moveDirection.normalized, transform.forward);
         float velocityX = Vector3.Dot(moveDirection.normalized, transform.right);
-
-        //Quaternion newDirection = Quaternion.LookRotation(movement);
 
         //pass velocity to animator vars
         _animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
          _animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
 
-        /*Rotation*/
+    }
 
+    private void Rotate()
+    {
         transform.Rotate(Vector3.up, lookVector.x * lookSensitivity * Time.deltaTime);
-
-
-
     }
 
     public void OnWalkUnarmed(InputAction.CallbackContext context)
@@ -84,14 +109,14 @@ public class PlayerController : MonoBehaviour
             _animator.SetBool("isWalkingUnarmed", true);
             _animator.SetBool("isJoggingUnarmed", false);
             _animator.SetBool("isSprintingUnarmed", false);
-            moveSpeed = 1.8f;
+            moveSpeed = walkSpeed;
         } 
         else
         {
             _animator.SetBool("isWalkingUnarmed", false);
             _animator.SetBool("isJoggingUnarmed", true);
             _animator.SetBool("isSprintingUnarmed", false);
-            moveSpeed = 4;
+            moveSpeed = jogSpeed;
         }
         
     }
@@ -104,14 +129,14 @@ public class PlayerController : MonoBehaviour
             _animator.SetBool("isWalkingUnarmed", false);
             _animator.SetBool("isJoggingUnarmed", false);
             _animator.SetBool("isSprintingUnarmed", true);
-            moveSpeed = 8;
+            moveSpeed = sprintSpeed;
         }
         else
         {
             _animator.SetBool("isWalkingUnarmed", false);
             _animator.SetBool("isJoggingUnarmed", true);
             _animator.SetBool("isSprintingUnarmed", false);
-            moveSpeed = 4;
+            moveSpeed = jogSpeed;
         }
 
     }
