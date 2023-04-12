03475b8318625f19e6925b07ea316688abf6f1c2
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-04-12 12:57:54 -0700

    MapGenerator.cs now has public values for octaves, lacunarity, and persistence.  New vars are added to the GenerateNoiseMap() call as well.

--- Assets/Environment/MapGeneration/MapGenerator.cs
+++ Assets/Environment/MapGeneration/MapGenerator.cs
@@ -9,11 +9,15 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    public float lacunarity;
+    public float persistance;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, lacunarity, persistance);
 
         MapDisplay display = FindObjectOfType<MapDisplay>(); //ref to MapDisplay.cs
         display.DrawNoiseMap(noiseMap); //create texture plane with a noise map
