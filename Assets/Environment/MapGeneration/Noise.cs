01ab32e19b161289ab30a82cbf69c286b5d0d9e8
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-04-12 12:54:40 -0700

    Added octaves, lacunarity, and persistance to GenerateNoiseMap() in Noise.cs.
    In the first pass of x and y values they are now multiplied by frequency and divided by noiseScale. Perlin noise range changed from 0->1 to -1->1. Resulting values from Perlin noise fxn are multiplied by amplitude then added and assigned to noiseHeight. Amplitude and frequency are then multiplied by persistence and lacunarity respectively. At the same time the min and max height values are recorded for the next pass where all noiseMap[x,y] values are normalized.

--- Assets/Environment/MapGeneration/Noise.cs
+++ Assets/Environment/MapGeneration/Noise.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class Noise
 {
     
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float noiseScale)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float noiseScale, int octaves, float lacunarity, float persistance)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
@@ -15,19 +15,60 @@ public static class Noise
             noiseScale = 0.0001f;
         }
 
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                float sampleX = x / noiseScale;
-                float sampleY = y / noiseScale;
 
-                float perlinVal = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinVal;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = x * frequency / noiseScale;
+                    float sampleY = y * frequency / noiseScale;
+
+                    float perlinVal = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1; // "* 2 - 1" changes range from [0,1] to [-1,1]
+                    noiseHeight += perlinVal * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                //keep track of min and max height values for normalization
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+
             }
         }
 
-        return noiseMap;
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                /*
+                 * Normalizes map by returning the relative position of the current height between 0 and 1.
+                 * Ex: height = minNoiseHeight return 0
+                 *     height = maxNoiseHeight return 1
+                 *     height = (minNoiseHeight + maxNoiseHeight)/2 return 0.5
+                 */
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+
+        }
+
+    return noiseMap;
 
     }
 
