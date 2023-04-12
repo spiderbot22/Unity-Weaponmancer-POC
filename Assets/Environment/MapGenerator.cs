ccef3c52db2a3ca10f228663ffb9faf32150ced9
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-04-12 11:49:09 -0700

    Updated MapGenerator.cs to use DrawNoiseMap() in MapDisplay.cs. for creating the texture for a plane object.

--- Assets/Environment/MapGenerator.cs
+++ Assets/Environment/MapGenerator.cs
@@ -12,6 +12,9 @@ public class MapGenerator : MonoBehaviour
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>(); //ref to MapDisplay.cs
+        display.DrawNoiseMap(noiseMap); //create texture plane with a noise map
     }
 
 }
