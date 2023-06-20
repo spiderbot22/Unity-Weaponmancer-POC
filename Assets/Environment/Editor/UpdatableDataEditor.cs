8d5606edaf40d228662bdd21cf222d3b75d1ece8
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-06-20 13:54:15 -0700

    Added some colors and height values to the corresponding arrays in the Texture Data asset, works well. Added a line in UpdatableDataEditor so that the texture data correctly shows after updating it when before a value had to be changed for it to load the data.

--- Assets/Environment/Editor/UpdatableDataEditor.cs
+++ Assets/Environment/Editor/UpdatableDataEditor.cs
@@ -16,6 +16,7 @@ public class UpdatableDataEditor : Editor
         if (GUILayout.Button("Update"))
         {
             data.NotifyOfUpdatedValues();
+            EditorUtility.SetDirty(target);
         }
     }
 
