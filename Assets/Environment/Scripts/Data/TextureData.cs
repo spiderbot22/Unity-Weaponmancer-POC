df3851e87e32649afeea3533e19f99acb21b46bb
spiderbot22 <33765579+spiderbot22@users.noreply.github.com>
2023-06-20 13:02:39 -0700

    Created terrain shader Terrain.shader. Edited TerrainData and TextureData to feed map heights per point into the shader so that the shader can fill in color depending on height. Currently produces a black and white gradient.

--- Assets/Environment/Scripts/Data/TextureData.cs
+++ Assets/Environment/Scripts/Data/TextureData.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class TextureData : UpdatableData
 {
    public void ApplyToMaterial(Material material)
     {
-
+      
     }
+
+    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
+    {
+        material.SetFloat("minHeight", minHeight);
+        material.SetFloat("maxHeight", maxHeight);
+    }
+
 }
